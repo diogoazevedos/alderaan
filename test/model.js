12bf56174ee690fde6d11f800b9639463d87d01e
Luan Motta <luanmotta97@hotmail.com>
2017-03-03 13:10:51 -0300

    test: Add model deletedAt specs

--- test/model.js
+++ test/model.js
@@ -29,6 +29,24 @@ test('should instantiate a model with database provided data', (t) => {
     id: 20,
     created_at: '2015-10-05 20:15:10',
     updated_at: '2015-10-05 10:15:20',
+    deleted_at: null,
+  };
+
+  const model = Model.of(attributes);
+
+  t.is(model.id, attributes.id);
+  t.is(model.deletedAt, null);
+  t.true(isMoment(model.updatedAt));
+  t.true(isMoment(model.createdAt));
+  t.deepEqual(model.attributes, attributes);
+});
+
+test('should instantiate a deleted model with database provided data', (t) => {
+  const attributes = {
+    id: 20,
+    created_at: '2015-10-05 20:15:10',
+    updated_at: '2015-10-05 10:15:20',
+    deleted_at: '2015-10-05 05:15:10',
   };
 
   const model = Model.of(attributes);
@@ -36,6 +54,7 @@ test('should instantiate a model with database provided data', (t) => {
   t.is(model.id, attributes.id);
   t.true(isMoment(model.updatedAt));
   t.true(isMoment(model.createdAt));
+  t.true(isMoment(model.deletedAt));
   t.deepEqual(model.attributes, attributes);
 });
 
@@ -58,6 +77,14 @@ test('should throw an error when createdAt is not a moment instance', (t) => {
 test('should throw an error when updatedAt is not a moment instance', (t) => {
   try {
     new Model({ updatedAt: 'foo' });
+  } catch (error) {
+    t.true(error instanceof TypeError);
+  }
+});
+
+test('should throw an type error when deletedAt is not a moment instance and neither nil', (t) => {
+  try {
+    new Model({ deletedAt: 'foo' });
   } catch (error) {
     t.true(error instanceof TypeError);
   }
