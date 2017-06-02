72132fd52606119000db27ac8e1212b7cb912d3d
Diogo Azevedo <diogoazevedos@gmail.com>
2017-06-02 12:44:26 -0300

    test: Bind repository assert functions

--- test/repository.js
+++ test/repository.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import knex from 'knex';
 import { utc } from 'moment';
-import { is, unless } from 'ramda';
+import { is, ifElse } from 'ramda';
 import { isNothing, isJust, maybeToNullable } from 'sanctuary';
 import config from './fixtures/database';
 import Model from '../lib/model';
@@ -18,24 +18,24 @@ test.beforeEach(() => (
 
 test.serial('should fetch all resources', t => (
   repository.all(db)
-    .then(unless(is(Array), t.fail))
+    .then(ifElse(is(Array), t.pass.bind(t), t.fail.bind(t)))
 ));
 
 test.serial('should find a resource and return a just instance', t => (
   repository.find(db, 1)
-    .then(unless(isJust, t.fail))
+    .then(ifElse(isJust, t.pass.bind(t), t.fail.bind(t)))
 ));
 
 test.serial('should not find a resource and return a nothing instance', t => (
   repository.find(db, 2)
-    .then(unless(isNothing, t.fail))
+    .then(ifElse(isNothing, t.pass.bind(t), t.fail.bind(t)))
 ));
 
 test.serial('should store and return a resource in storage', (t) => {
   const model = new Model();
 
   return repository.create(db, model)
-    .then(unless(is(Model), t.fail));
+    .then(ifElse(is(Model), t.pass.bind(t), t.fail.bind(t)));
 });
 
 test.serial('should update a specific resource in storage', async (t) => {
@@ -51,5 +51,5 @@ test.serial('should update a specific resource in storage', async (t) => {
 
 test.serial('should delete a specific resource in storage', t => (
   repository.destroy(db, 1)
-    .then(unless(is(Number), t.fail))
+    .then(ifElse(is(Number), t.pass.bind(t), t.fail.bind(t)))
 ));
